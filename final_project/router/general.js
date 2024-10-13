84c78f9279ab195ecac8e5a53099072330ae2cc3
YAMANX8 <80436506+YAMANX8@users.noreply.github.com>
2024-10-13 22:03:54 +0300

    task 6 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,8 +5,19 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { username, password } = req.body;
+
+  if (!username || !password)
+    return res
+      .status(400)
+      .json({ message: "username and password are required" });
+
+  const userExists = users.some((user) => user.username === username);
+  if (userExists)
+    return res.status(400).json({ message: "Username already exists" });
+
+  users.push({ username, password });
+  return res.status(200).json({ message: "User registered successfully" });
 });
 
 // Get the book list available in the shop
